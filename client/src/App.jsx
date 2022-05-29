09c63fd7bb86f0617d09cad02e074e7905fcbe69
krishna <keshri819@gmail.com>
2022-05-29 12:52:25 +0530

    Created Arrow Component

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import BtnArrow from "./Components/BtnArrow";
+function App() {
+  return (
+    <>
+      <BtnArrow />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Components/BtnArrow.jsx
@@ -0,0 +1,11 @@
+import arrow from "../Images/109618.png";
+
+const BtnArrow = () => {
+  return (
+    <button className="btn-arrow">
+      <img src={arrow} alt="arrow" />
+    </button>
+  );
+};
+
+export default BtnArrow;
