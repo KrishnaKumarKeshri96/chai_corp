565f8671fb2e6546ba38646f5f879eed74b54936
krishna <keshri819@gmail.com>
2022-05-29 12:59:22 +0530

    Checkout Div Component Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import BtnArrow from "./Components/BtnArrow";
+import CheckOut from "./Components/CheckOut";
 function App() {
   return (
     <>
       <BtnArrow />
+      <CheckOut />
     </>
   );
 }
